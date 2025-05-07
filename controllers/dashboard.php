68c70703d2e2ee0287c2848110d890b2dec221e0
Abhishek <abhishek.das0023@gmail.com>
2025-05-08 02:03:42 +0600

    update: session set

--- controllers/dashboard.php
+++ controllers/dashboard.php
@@ -1,5 +1,8 @@
 <?php
-session_start(); // Ensure session is started
+
+require_once __DIR__ . '/../services/session.php';
+startSession(); // Ensure session is started
+
 require_once __DIR__ . '/../services/auth_check.php';
 requireLogin();
 
