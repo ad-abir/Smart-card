0d2e8bed0f90aa28b031531f410a3a03e74a88d4
Abhishek <abhishek.das0023@gmail.com>
2025-06-25 00:54:53 +0600

    update: separate call for preview page.

---
+++ controllers/preview_page.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . '/../services/session.php';
+startSession(); // Ensure session is started
+
+require_once __DIR__ . '/../services/auth_check.php';
+requireLogin();
+
+// Include the view
+include __DIR__ . '/../views/preview.php';
+?>
